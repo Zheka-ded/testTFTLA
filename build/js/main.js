23e6a14f3f9917d255d839b0b6e7fc1db6655344
Zheka-ded <11vitamin11@rambler.ru>
2021-01-11 08:04:25 +0200

    almost all

--- build/js/main.js
+++ build/js/main.js
@@ -142,3 +142,10 @@ function slidePrevFunk (arr, cssClass, indexCssClass) {
     }
     
 }
+const rightBtn = document.querySelector('.right__btn');
+const nav = document.querySelector('.right__nav');
+
+rightBtn.onclick = function() {
+    rightBtn.classList.toggle('right__btn-active')
+    nav.classList.toggle('right__nav-active')
+}
